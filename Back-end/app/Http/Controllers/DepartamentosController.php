fcb862713a53e1ccdf05ffdac24d698d2f9c80be
GenjiHD <joaquincastrosarmientosanchez@gmail.com>
2025-03-20 13:56:56 -0700

    falta las operaciones CRUD para reporte movimiento

--- Back-end/app/Http/Controllers/DepartamentosController.php
+++ Back-end/app/Http/Controllers/DepartamentosController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Departamentos;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class DepartamentosController extends Controller
 {
@@ -20,5 +21,128 @@ class DepartamentosController extends Controller
             return response()->json($data, 404);
         }
         return response()->json($departamentos, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'Descripcion' => 'required'
+        ]);
+
+        try {
+            $departamento = Departamentos::create($validateData);
+            $data = [
+                'Departamentos' => $departamento,
+                'message' => 'El departamento se creo correctamente',
+                'error' => 200
+            ];
+        } catch (\Exception $e) {
+            $data = [
+                'message' => 'Error al crear el departamento: ',
+                $e->getMessage(),
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $departamento = Departamentos::find($id);
+
+        if (!$departamento) {
+            return response()->json([
+                'message' => 'Departamento no encontrado',
+                'status' => 404
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'Descripcion' => 'sometimes'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Error de validación',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ], 400);
+        }
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Error de validación',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ], 400);
+        }
+
+        // Actualizar solo los campos proporcionados en la solicitud
+        $departamento->fill($validator->validated());
+
+        // Guardar los cambios en la base de datos
+        $departamento->save();
+
+        // Retornar una respuesta exitosa
+        return response()->json([
+            'message' => 'Departamento actualizado correctamente',
+            'Producto' => $departamento,
+            'status' => 200
+        ], 200);
+    }
+
+    public function destroy($id)
+    {
+        $departamento = Departamentos::find($id);
+
+        if (!$departamento) {
+            $data = [
+                'message' => 'Departamento no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $departamento->delete();
+
+        $data = [
+            'message' => 'Departamento eliminado correctamente',
+            'status' => 200
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function buscar(Request $request)
+    {
+
+        $termino = $request->query('q');
+
+
+        if (empty($termino)) {
+            return response()->json([
+                'message' => 'Debe proporcionar un término de búsqueda',
+                'status' => 400
+            ], 400);
+        }
+
+
+        $departamento = Departamentos::where('DeptoID', 'like', "%$termino%")
+            ->orWhere('Descripcion', 'like', "%$termino%")
+            ->get();
+
+
+        if ($departamento->isEmpty()) {
+            return response()->json([
+                'message' => 'No se encontro el departamento',
+                'status' => 404
+            ], 404);
+        }
+
+
+        return response()->json([
+            'message' => 'Departamento encontrado',
+            'productos' => $departamento,
+            'status' => 200
+        ], 200);
     }
 }
--- Back-end/app/Http/Controllers/TipoMovimientoController.php
+++ Back-end/app/Http/Controllers/TipoMovimientoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\TipoMovimiento;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class TipoMovimientoController extends Controller
@@ -20,5 +21,83 @@ class TipoMovimientoController extends Controller
             return response()->json($data, 404);
         }
         return response()->json($tipoMovimiento, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'Descripcion' => 'required'
+        ]);
+
+        try {
+            $tipomov = TipoMovimiento::create($validateData);
+
+            $data = [
+                'Tipo movimiento' => $tipomov,
+                'message' => 'El tipo de movimiento fue creado correctamente',
+                'status' => 200
+            ];
+            return response()->json($data, 200);
+        } catch (\Exception $e) {
+            $data = [
+                'message' => 'El tipo de movmiento no pudo ser creado:' . $e->getMessage(),
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $tipomov = TipoMovimiento::find($id);
+
+        if (!$tipomov) {
+            return response()->json([
+                'message' => 'El tipo de movmiento no fue encontrado',
+                'status' => 404
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'Descripcion' => 'sometimes',
+        ]);
+
+        if (!$validator->fails()) {
+            return response()->json([
+                'message' => 'Error de validacion',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ], 400);
+        }
+
+        $tipomov->fill($validator->validated());
+        $tipomov->save();
+
+        return response()->json([
+            'message' => 'El movimiento ha sido actualizado correctamente',
+            'Tipo Movimiento' => $tipomov,
+            'status' => 200
+        ], 200);
+    }
+
+    public function destroy($id)
+    {
+        $tipomov = TipoMovimiento::find($id);
+
+        if (!$tipomov) {
+            $data = [
+                'message' => 'El tipo de movimiento no fue encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $tipomov->delete();
+
+        $data = [
+            'message' => 'El tipo de movimiento fue eliminado correctamente',
+            'status' => 200
+        ];
+        return response()->json($data, 200);
     }
 }
