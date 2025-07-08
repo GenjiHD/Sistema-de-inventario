4dbd2e2ae782c8b5e2b0a7c334e91b69b6b5cc05
GenjiHD <joaquincastrosarmientosanchez@gmail.com>
2025-07-08 14:01:30 -0700

    fix: do testing and logic for users started, dude come on you can finish this shit just do it

--- Front-end/src/app/components/lista-usuarios/lista-usuarios.component.ts
+++ Front-end/src/app/components/lista-usuarios/lista-usuarios.component.ts
@@ -1,12 +1,40 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { UsuariosService } from '../services/usuarios.service';
+import { Usuarios } from '../../models/usuarios.model''
 
 @Component({
   selector: 'app-lista-usuarios',
+  standalone: true,
   imports: [],
   templateUrl: './lista-usuarios.component.html',
   styleUrl: './lista-usuarios.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ListaUsuariosComponent {
 
+export class ListaUsuariosComponent {
+  usuarios: Usuarios[] = [];
+
+  loading: boolean = true;
+
+  constructor(private usuariosService: UsuariosService) {}
+
+  ng OnInit(): void {
+    this.cargarUsuarios();
+  }
+
+  cargarUsuarios(): void {
+    this.loading = true;
+    this.usuariosService.getUsuarios().subscribe({
+      next: (data: Usuarios[]) => {
+        this.usuarios = data;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error al cargar los usuarios: ', error);
+        this.loading = false;
+      }
+    });
+  }
 }
--- Front-end/src/app/components/services/usuarios.service.ts
+++ Front-end/src/app/components/services/usuarios.service.ts
@@ -9,4 +9,26 @@ import { Usuarios } from '../../models/usuarios.model';
 
 export class UsuariosService {
   private apiUrl = 'http://127.0.0.1:8000/api/usuarios';
+
+  constructor(private http: HttpClient) { }
+
+  getUsuarios(): Observable<Usuarios[]> {
+  return this.http.get<Usuarios[]>(this.apiUrl);
+  }
+
+  createUsuario(usuarios: Omit<Usuarios,'UsuarioID'>): Observable<Usuarios> {
+    return this.http.post<Usuarios>(this.apiUrl, usuarios);
+  }
+
+  updateUsuario(usuarios: Usuarios): Observable<Usuarios> {
+  const url = `${this.apiUrl}/${usuario.UsuarioID}`;
+
+    const body = {
+      Nombre: usuarios.Nombre,
+      Contraseña: usuarios.Contraseña,
+      Puesto: usuarios.Puesto,
+      Estado: usuarios.Estado
+    };
+    return this.http.put<Usuarios>(url, body);
+  }
 }
